d2b204e9275ec30c5e058bec89c1e1875ca9e32a
Vyacheslav Palamar <vyacheslav.palamar@gmail.com>
2016-03-06 13:53:52 +0200

    checkUserInput modified for more wrong cases.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var buttonApply = document.getElementById('button_apply');
 
-var validateUserInput = function validateUserInputF (input) {
+var checkUserInput = function checkUserInputF (input) {
+  if (!input) return false;
+  if (input.constructor !== Array || input.length == 0) return false;
   for (var i = 0; i < input.length; i++) {
       if (!(!isNaN(parseFloat(input[i])) && isFinite(input[i]))) {
         return false;
