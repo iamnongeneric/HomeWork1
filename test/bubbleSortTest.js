58a9dffb6dbafbbd3ae69b51f8809c2622fd4f44
Vyacheslav Palamar <vyacheslav.palamar@gmail.com>
2016-03-06 11:52:33 +0200

    Modified bubble sort test,now it returns object

--- test/bubbleSortTest.js
+++ test/bubbleSortTest.js
@@ -1,7 +1,7 @@
 var expect = chai.expect;
 
 describe('What bubble sort function returns', function () {
-  it('should return a function which allows to control the bubble sort execution', function () {
-    expect(bubbleSort([5,4,3,2,1])).to.be.a('function');
-  })
-})
+  it('should return an object which allows to control the bubble sort execution', function () {
+    expect(bubbleSort([5, 4, 3, 2, 1])).to.be.a('object');
+  });
+});
