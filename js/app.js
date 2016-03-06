3144532bfc476591f27aa7880247bcfa8333b946
Vyacheslav Palamar <vyacheslav.palamar@gmail.com>
2016-03-06 14:32:34 +0200

    Added some functions which transform input to ul.

--- js/app.js
+++ js/app.js
@@ -10,3 +10,31 @@ var checkUserInput = function checkUserInputF (input) {
   }
   return true;
 }
+
+var setUserInputView = function setUserInputViewF (input) {
+  var viewSection = document.getElementById('nums_list');
+  input.forEach(function (item) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+    viewSection.appendChild(li);
+  })
+}
+
+var respondToUserInput = function respondToUserInputF() {
+  var input = document.getElementById('nums_input').value.split(" ");
+  var executionSection = document.getElementById('execution_section');
+  var errorMessage = document.getElementById('error_message');
+  if (checkUserInput(input)) {
+    //create li elements from input and set execustion section visible
+    button_apply.disabled = true;
+    setUserInputView(input);
+    executionSection.style.display = 'block';
+    errorMessage.style.display = 'none';
+  } else {
+    //show error section
+    errorMessage.style.display = 'block';
+  }
+}
+
+
+buttonApply.onclick = respondToUserInput;
