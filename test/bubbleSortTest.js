14694688ec4c665f42d05fefe0c293106ce3ba62
Vyacheslav Palamar <vyacheslav.palamar@gmail.com>
2016-03-06 12:13:46 +0200

    Modified tests for new behavior of returned object.

--- test/bubbleSortTest.js
+++ test/bubbleSortTest.js
@@ -5,3 +5,26 @@ describe('What bubble sort function returns', function () {
     expect(bubbleSort([5, 4, 3, 2, 1])).to.be.a('object');
   });
 });
+
+describe('Properties of returned object', function () {
+  var testObj = bubbleSort([11, 32, 0, 11, -1, 7]); //random
+  it('should have a loop property which is a function', function () {
+    expect(testObj.loop).to.be.a('function');
+  });
+  it('should have a getCurrentElement property, counter of the inner loop',
+  function () {
+    expect(testObj.getCurrentElement()).to.be.a('number');
+  });
+  it('should have a getCurrentEndOfLoop property, counter of the outer loop',
+  function () {
+    expect(testObj.getCurrentEndOfLoop()).to.be.a('number');
+  });
+});
+
+describe('Check the work of loop function', function () {
+  var testObj = bubbleSort([30, 5, 7, 0, -3, 1, -10]);
+  testObj.loop();
+  it('should increase value of currentElement by one', function () {
+    expect(testObj.getCurrentElement()).to.equal(1);
+  });
+});
