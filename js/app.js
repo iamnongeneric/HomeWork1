bf7018615263a746148c1ea6296d853664ef9947
Vyacheslav Palamar <vyacheslav.palamar@gmail.com>
2016-03-06 17:48:20 +0200

    Changes in array are visible in HTML.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 var buttonApply = document.getElementById('button_apply');
+var buttonNext = document.getElementById('button_next');
+var viewSection, bubbleSortObject;
 
 var checkUserInput = function checkUserInputF (input) {
   if (!input) return false;
@@ -11,30 +13,41 @@ var checkUserInput = function checkUserInputF (input) {
   return true;
 }
 
-var setUserInputView = function setUserInputViewF (input) {
-  var viewSection = document.getElementById('nums_list');
+var setListView = function setListViewF (input) {
+  viewSection = document.getElementById('nums_list');
   input.forEach(function (item) {
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(item));
     viewSection.appendChild(li);
-  })
+  });
 }
 
 var respondToUserInput = function respondToUserInputF() {
-  var input = document.getElementById('nums_input').value.split(" ");
+  var input = document.getElementById('nums_input').value.trim().split(" ");
+  input = input.filter(function (item) {
+    return /\S/.test(item);
+  });
   var executionSection = document.getElementById('execution_section');
   var errorMessage = document.getElementById('error_message');
   if (checkUserInput(input)) {
     //create li elements from input and set execustion section visible
+    bubbleSortObject = bubbleSort(input);
+    setListView(input);
     button_apply.disabled = true;
-    setUserInputView(input);
     executionSection.style.display = 'block';
     errorMessage.style.display = 'none';
   } else {
     //show error section
+    alert(input);
     errorMessage.style.display = 'block';
   }
 }
 
+var controlListChanges = function controlListChangesF () {
+  viewSection.innerHTML = "";
+  bubbleSortObject.loop();
+  setListView(bubbleSortObject.getList());
+}
 
 buttonApply.onclick = respondToUserInput;
+buttonNext.onclick = controlListChanges;
